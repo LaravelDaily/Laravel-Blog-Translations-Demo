5198b025af9040ec46e40409884cec16d5097712
Marius Kasparavičius <mariuskasparavicius@inbox.lt>
2020-03-09 18:28:13 +0200

    User seed & removed register

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
